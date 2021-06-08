56f9cd3d9907bfaf995955b25926b9a1a61edf04
canosaer <canosaer@gmail.com>
2021-06-08 14:26:10 -0400

    1.1

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ const toggleMenuElements = {
     toggleLines: document.querySelectorAll(`.toggle__line`)
 }
 
+const galleryItems = document.querySelectorAll('.gallery__item')
+
 class MenuToggle{
     elements = null
     
@@ -13,8 +15,6 @@ class MenuToggle{
         this.siteNavigation = elements.siteNavigation
         this.siteNavigationList = elements.siteNavigationList
         this.toggleLines = elements.toggleLines
-
-        
 
         this.setupListeners()
     }
@@ -50,4 +50,69 @@ class MenuToggle{
     }
 }
 
+class ImageGallery{
+    constructor(items){
+        this.imageSlots = items
+
+        this.setupListeners()
+    }
+
+    setupListeners() {
+
+        this.imageSlots.forEach(imageSlot => {
+            imageSlot.addEventListener(`mouseover`, this.beginGallerySlotHover)
+            imageSlot.addEventListener(`mouseout`, this.endGallerySlotHover)
+        });
+    }
+
+    beginGallerySlotHover = (evt) => {
+        if(window.innerWidth < 768){
+            for(let i=0;i<this.imageSlots.length;i++){
+                if(this.imageSlots[i].querySelector('img') === evt.target){
+                    this.imageSlots[i].style.height = '35rem'
+                }
+            }
+        }
+        else{
+            for(let i=0;i<this.imageSlots.length;i++){
+                if(this.imageSlots[i].querySelector('img') === evt.target){
+                    this.imageSlots[i].style.width = '80%'
+                    this.imageSlots[i].style.height = '19rem'
+                }
+                else{
+                    this.imageSlots[i].style.width = '20%'
+                    this.imageSlots[i].style.height = 'unset'
+                } 
+            }
+        }
+        
+    }
+
+    endGallerySlotHover = (evt) => {
+        if(window.innerWidth < 768){
+            for(let i=0;i<this.imageSlots.length;i++){
+                if(this.imageSlots[i].querySelector('img') === evt.target){
+                    this.imageSlots[i].style.height = '0'
+                }
+            }
+        }
+        
+    }
+
+
+}
+
 const toggleMenu = new MenuToggle(toggleMenuElements)
+
+const galleryHandler = new ImageGallery(galleryItems)
+
+function fixItems() {
+
+    if(window.innerWidth < 768){
+        galleryItems.forEach(item => {
+            item.style.width = '100%'
+        });
+    }
+}
+
+window.onresize = fixItems;
